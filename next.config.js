836f69385682af26c454af40bdf9fc78f52d1d48
Michael Anderson <michael.j.anderson@me.com>
2023-11-05 05:43:24 -0500

    Trying again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: { 
+    BING_COOKIE: process.env.BING_COOKIE,
+    BING_VARIANT: process.env.BING_VARIANT
+  }
 }
 
 module.exports = nextConfig
